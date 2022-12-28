a7751d3acd9d92081b18273d574851e89310aa4e
Evan Liu <4542elgh@gmail.com>
2022-12-28 12:52:32 -0800

    Take user define compiler

--- lua/telescope/_extensions/scratch_run.lua
+++ lua/telescope/_extensions/scratch_run.lua
@@ -1,3 +1,11 @@
+-- Check telescope is installed
+local ok, telescope = pcall(require, 'telescope')
+
+if not ok then
+    error 'Install nvim-telescope/telescope.nvim to use 4542elgh/telescope-scratch-run.nvim.'
+end
+
+-- Telescope utils
 local finders = require('telescope.finders')
 local pickers = require('telescope.pickers')
 local entry_display = require('telescope.pickers.entry_display')
@@ -6,45 +14,31 @@ local conf = require('telescope.config').values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
-local function make_repl()
-    local repl = {
-        pyton = {
-            name = "python",
-            cmd = ":w !python"
-        },
-        node = {
-            name = "node",
-            cmd = ":w !node"
-        },
-        java = {
-            name = "java",
-            cmd = ":w !javac"
-        },
-        golang = {
-            name = "golang",
-            cmd = ":w !go run"
-        },
-        rustlang = {
-            name = "rustlang",
-            cmd = ":w !rustc "
-        },
-    }
+-- Predefined list
+local compilers = {
+    python = { name = "python", cmd = ":w !python" },
+    node = { name = "node", cmd = ":w !node" },
+    java = { name = "java", cmd = ":w !javac" },
+    golang = { name = "golang", cmd = ":w !go run" },
+    rustlang = { name = "rustlang", cmd = ":w !rustc " },
+}
 
+-- Make format better
+local function make_compiler()
     local languagesPicker = {}
-    local count = 0
-
-    for _,val in pairs(repl) do
-        count = count + 1
+    for _, val in pairs(compilers) do
         table.insert(languagesPicker, {
             lang = val.name,
             cmd = val.cmd,
         })
     end
-
     return languagesPicker
 end
 
+-- This is what will be showing in Telescope
 local function make_entry()
+ 
+    -- Spacing
     local displayer = entry_display.create {
         separator = "",
         items = {
@@ -53,6 +47,7 @@ local function make_entry()
         }
     }
 
+    -- What content is displaying
     local make_display = function(entry)
         return displayer {
             entry.lang,
@@ -60,6 +55,7 @@ local function make_entry()
         }
     end
 
+    -- Internal sorting
     return function(entry)
         return {
             value = entry,
@@ -71,18 +67,22 @@ local function make_entry()
     end
 end
 
+-- Finder will fill picker with items
 local make_finder = function()
     return finders.new_table {
-        results = make_repl(),
+        results = make_compiler(),
         entry_maker = make_entry(),
     }
 end
 
+-- Displaying module, putting everything together
 local function make_picker()
     pickers.new({}, {
         prompt_title = "Complier Language",
         finder = make_finder(),
         sorter = conf.generic_sorter({}),
+
+        -- What to do with selected item
         attach_mappings = function(prompt_bufnr)
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
@@ -93,9 +93,12 @@ local function make_picker()
     }):find()
 end
 
-return require('telescope').register_extension {
+return require("telescope").register_extension {
+    setup = function(user_opts, _)
+        compilers = vim.tbl_extend('force', compilers, user_opts.custom_compilers)
+    end,
     exports = {
         scratch_run = make_picker,
-        all = make_picker,
+        all = make_picker
     }
 }
